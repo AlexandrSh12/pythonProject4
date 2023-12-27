1e9ebcac81daa528667c3e6a87dfbbee93705b19
AlexandrSh12 <alexandr.shapovalov12@gmail.com>
2023-12-27 22:53:56 +0700

    Final2

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"data": "ФИО: Шаповалов Александр Иванович"}
+    return {"FIO": "Шаповалов Александр Иванович"}
 
 @app.get("/users")
 def read_users():
-    return {"data": "Контактные данные: 8-800-555-3535, улица Герцена 94"}
+    return {"Контактные данные": " 8-800-555-3535, улица Герцена 94"}
 
 @app.get("/tools")
 def read_tools():
-    return {"data": "Навыки разработчика: Haskel, C#, C, SQL..."}
+    return {"Навыки разработчика": "Haskel, C#, C, SQL..."}
 
